65bf567456e19d0da0e332722a824da532b54956
Hamburger <chrischenghc@gmail.com>
2020-01-13 16:01:27 +0800

    complete number of button

--- DelegatePractice/SelectionView.swift
+++ DelegatePractice/SelectionView.swift
@@ -19,17 +19,32 @@ protocol SelectionViewDataSource: AnyObject {
     
     func textOnButton(_ selectionView: SelectionView) -> String
     
-    func colorOfUnderLine(_ selectionView: SelectionView) -> UIColor
+    func colorOfUnderLine(_ selectionView: SelectionView) -> UIColor?
     
-    func colorOfTextOnButton(_ selectionView: SelectionView)
+    func colorOfTextOnButton(_ selectionView: SelectionView) -> UIColor?
     
-    func fontOfTextOnButton(_ selectionView: SelectionView)
+    func fontOfTextOnButton(_ selectionView: SelectionView) -> UIFont?
 }
 
 class SelectionView: UIView {
     
-    weak var delegate: SelectionViewDelegate?
-    weak var dataSource: SelectionViewDataSource?
+    weak var delegate: SelectionViewDelegate? {
+        
+        didSet {
+            setButton()
+        }
+        
+    }
+    
+    weak var dataSource: SelectionViewDataSource? {
+        
+        didSet {
+            setButton()
+        }
+        
+    }
+    
+    var arrayButton = [UIButton]()
     
     let colorView: UIView = {
         let view = UIView()
@@ -43,43 +58,76 @@ class SelectionView: UIView {
         return stack
     }()
     
-    let optionButton: UIButton = {
-        let btn = UIButton()
-        btn.setTitleColor(.white, for: .normal)
-        btn.isEnabled = true
-        btn.backgroundColor = .black
-        btn.titleLabel?.font = UIFont.systemFont(ofSize: 18)
-        return btn
-    }()
-    
     let indicatorView: UIView = {
         let view = UIView()
         view.backgroundColor = .blue
         return view
     }()
     
+    func setColorView() {
+        colorView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(colorView)
+        NSLayoutConstraint.activate([
+            NSLayoutConstraint(item: colorView, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1, constant: 50),
+            NSLayoutConstraint(item: colorView, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: colorView, attribute: .leading, relatedBy: .equal, toItem: self, attribute: .leading, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: colorView, attribute: .trailing, relatedBy: .equal, toItem: self, attribute: .trailing, multiplier: 1, constant: 0)
+        ])
+    }
+    
     func setStackView() {
         stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
         stackView.distribution = UIStackView.Distribution.fillEqually
-        stackView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        stackView.spacing = 0
+        addSubview(stackView)
+        //stackView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        NSLayoutConstraint.activate([
+            NSLayoutConstraint(item: stackView, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: stackView, attribute: .bottom, relatedBy: .equal, toItem: colorView, attribute: .top, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: stackView, attribute: .leading, relatedBy: .equal, toItem: self, attribute: .leading, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: stackView, attribute: .trailing, relatedBy: .equal, toItem: self, attribute: .trailing, multiplier: 1, constant: 0)
+        ])
     }
     
-    func setButtonAndIndicator() {
+    func setButton() {
+        guard let numberOfButton = dataSource?.numberOfOption(self) else { return }
+        for _ in 0...numberOfButton - 1 {
+            let optionButton: UIButton = {
+                let btn = UIButton()
+                btn.setTitleColor(.white, for: .normal)
+                btn.isEnabled = true
+                btn.backgroundColor = .yellow
+                btn.layer.borderColor = UIColor.gray.cgColor
+                btn.layer.borderWidth = 2
+                btn.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+                return btn
+            }()
+            arrayButton.append(optionButton)
+            stackView.addArrangedSubview(optionButton)
+        }
+    }
+    
+    func setIndicatorLine() {
         indicatorView.translatesAutoresizingMaskIntoConstraints = false
-        optionButton.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(indicatorView)
+        
+        NSLayoutConstraint.activate([
+            NSLayoutConstraint(item: indicatorView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 2),
+            NSLayoutConstraint(item: indicatorView, attribute: .bottom, relatedBy: .equal, toItem: colorView, attribute: .top, multiplier: 1, constant: -2)
+        ])
     }
     
-    
-    
-}
-
-
-class View: UIViewController, UITableViewDataSource, UITableViewDelegate{
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setColorView()
+        setStackView()
+        setButton()
+        setIndicatorLine()
+        self.backgroundColor = .black
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
--- DelegatePractice/ViewController.swift
+++ DelegatePractice/ViewController.swift
@@ -9,10 +9,80 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let topSelectionView = SelectionView()
+    let bottomSelectionView = SelectionView()
+    
+    func setSelectionView() {
+        view.addSubview(topSelectionView)
+        view.addSubview(bottomSelectionView)
+        topSelectionView.translatesAutoresizingMaskIntoConstraints = false
+        bottomSelectionView.translatesAutoresizingMaskIntoConstraints = false
 
+        NSLayoutConstraint.activate([
+            NSLayoutConstraint(item: topSelectionView, attribute: .top, relatedBy: .equal, toItem: self.view, attribute: .top, multiplier: 1, constant: 100),
+            NSLayoutConstraint(item: topSelectionView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 250),
+            NSLayoutConstraint(item: topSelectionView, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: topSelectionView, attribute: .trailing, relatedBy: .equal, toItem: self.view, attribute: .trailing, multiplier: 1, constant: 0)
+        ])
+        
+        NSLayoutConstraint.activate([
+            NSLayoutConstraint(item: bottomSelectionView, attribute: .top, relatedBy: .equal, toItem: topSelectionView, attribute: .bottom, multiplier: 1, constant: 50),
+            NSLayoutConstraint(item: bottomSelectionView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 250),
+            NSLayoutConstraint(item: bottomSelectionView, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: bottomSelectionView, attribute: .trailing, relatedBy: .equal, toItem: self.view, attribute: .trailing, multiplier: 1, constant: 0)
+        ])
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setSelectionView()
+        topSelectionView.delegate = self
+        topSelectionView.dataSource = self
+        bottomSelectionView.delegate = self
+        bottomSelectionView.dataSource = self
     }
+}
+
+extension ViewController: SelectionViewDelegate, SelectionViewDataSource {
+    func numberOfOption(_ selectionView: SelectionView) -> Int {
+        var number = 0
+        if selectionView == topSelectionView {
+            number = 2
+        } else if selectionView == bottomSelectionView {
+            number = 3
+        }
+        return number
+    }
+    
+    func textOnButton(_ selectionView: SelectionView) -> String {
+        if selectionView == topSelectionView {
+            return "1"
+        }
+        return "1"
+    }
+    
+    func colorOfUnderLine(_ selectionView: SelectionView) -> UIColor? {
+        if selectionView == topSelectionView {
+            return .red
+        }
+        return .red
+    }
+    
+    func colorOfTextOnButton(_ selectionView: SelectionView) -> UIColor? {
+        if selectionView == topSelectionView {
+            return .red
+        }
+        return .red
+    }
+    
+    func fontOfTextOnButton(_ selectionView: SelectionView) -> UIFont? {
+        if selectionView == topSelectionView {
+            return .systemFont(ofSize: 18)
+        }
+        return .systemFont(ofSize: 18)
+    }
+    
+    
 }
 
