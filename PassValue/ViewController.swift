530c7b7afae6ca75a6ccf3e4eaf2885172135277
Hamburger <chrischenghc@gmail.com>
2020-01-15 09:55:32 +0800

    Initial Commit

---
+++ PassValue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PassValue
+//
+//  Created by Hamburger on 2020/1/15.
+//  Copyright © 2020 Hamburger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
