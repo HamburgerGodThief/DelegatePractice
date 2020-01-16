696800368e3582a67c57546101afa5830f8ee8a7
Hamburger <chrischenghc@gmail.com>
2020-01-16 11:09:50 +0800

    Initial Commit

---
+++ AutoLayoutEventCycle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutEventCycle
+//
+//  Created by Hamburger on 2020/1/16.
+//  Copyright © 2020 Hamburger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
